63d03991baf8f6ad9a033e846bca6ef17b1aec41
dillonmorris91 <dillonmorris91@gmail.com>
2020-02-21 19:02:12 -0700

    Added filtering

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react"
+import React, { useState, useContext, useEffect } from "react"
 import { Link } from "gatsby"
 import { OrderContext } from "../context/OrderContext"
 import { defaultOrderState } from "../context/OrderContext"
@@ -7,6 +7,8 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Meal from "../components/Meal"
 import OrderTotals from "../components/OrderTotals"
+
+const filters = ["soup", "side", "paleo", "vegetarian", "vegan"]
 
 const IndexPage = () => {
   const {
@@ -17,6 +19,16 @@ const IndexPage = () => {
     mealCount,
     setMealCount,
   } = useContext(OrderContext)
+
+  const [activeFilter, setActiveFilter] = useState(null)
+
+  const updateFilter = filter => {
+    if (activeFilter && activeFilter === filter) {
+      setActiveFilter(null)
+    } else {
+      setActiveFilter(filter)
+    }
+  }
 
   useEffect(() => {
     const totals = meals.reduce(
@@ -37,7 +49,7 @@ const IndexPage = () => {
         style={{ padding: `1.0875rem` }}
         className="flex-col flex-1 max-w-xl font-sans container mx-auto"
       >
-        <div className="flex justify-between border-b border-gray-400 pb-4 mb-12">
+        <div className="flex justify-between pb-4 mb-2">
           <h1 className="text-2xl self-end text-gray-700 px-12 pt-5">Menu</h1>
           {mealCount > 0 && (
             <button
@@ -48,16 +60,34 @@ const IndexPage = () => {
             </button>
           )}
         </div>
+        {/* TODO: make filter buttons (Soup, Side, Paleo, Veggie, Vegan) */}
+        <div className="px-12">
+          {filters.map(filter => (
+            <button
+              key={filter}
+              className="px-4 py-1 rounded-full mr-2 capitalize text-indigo-900 bg-indigo-100"
+              onClick={() => updateFilter(filter)}
+              style={{
+                background: activeFilter === filter ? "#5A67D8" : "#EDF2F7",
+                color: activeFilter === filter ? "#fff" : "#4A5568",
+              }}
+            >
+              {filter}
+            </button>
+          ))}
+        </div>
         <div>
           {meals &&
-            meals.map(meal => (
-              <Meal
-                key={meal.id}
-                meal={meal}
-                meals={meals}
-                quantity={meal.quantity}
-              />
-            ))}
+            meals
+              .filter(meal => (activeFilter ? meal[activeFilter] : meal))
+              .map(meal => (
+                <Meal
+                  key={meal.id}
+                  meal={meal}
+                  meals={meals}
+                  quantity={meal.quantity}
+                />
+              ))}
         </div>
         <div style={{ maxWidth: "20rem", margin: "auto" }}>
           <OrderTotals subTotal={subTotal} mealCount={mealCount} />
